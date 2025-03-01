154d924ed1e30140f2d1c82316b90d633bc639b1
alex.mosisa <alex.mosisa@chisw.com>
2025-03-02 01:53:38 +0200

    sdf

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,7 +1,7 @@
 class AppStrings {
   static const String startButton = 'Старт';
   static const String nextButton = 'Далі';
-  static const String loadingText = 'завантаження!';
+  static const String loadingText = 'завантаження ...';
   static const String welcomeSubtitle =
       'Обирай товар, який тобі більше подобається';
   static const String orText = 'або';
